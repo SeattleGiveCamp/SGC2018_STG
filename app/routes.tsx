f9bc67c22a1f3a98d03ea10a35e3022dbf69067a
Donnie <donnie86@gmail.com>
2018-10-27 12:24:52 -0700

    Adding Camera QR test page

--- app/routes.tsx
+++ app/routes.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 import { Route } from 'mobx-router';
 
 import HomeContainer from './containers/HomeContainer';
+import QRReaderContainer from './containers/QRReaderContainer';
 
 const routes = {
 
 	home: new Route({
 		path: '/',
 		component: <HomeContainer />,
+	}),
+	qrreader: new Route({
+		path: '/qrreader',
+		component: <QRReaderContainer />,
 	})
 
 };
