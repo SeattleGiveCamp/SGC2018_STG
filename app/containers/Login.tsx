6a0e49c13e4453819ac36cc7a0e877d7b07e6b37
audreyandoy <audreylandoy@gmail.com>
2018-10-28 11:27:34 -0700

    finished front page

--- app/containers/Login.tsx
+++ app/containers/Login.tsx
@@ -37,43 +37,55 @@ export default class Login extends React.Component<{}, State> {
     console.log("checking");
   }
 
-
-
   render() {
     return (
       <div className="Login">
-      <div className="splash">
-        <img className="login_img" src={login_img} />
-      </div>
-      <div className="container">
-        <form className="login_form" onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel className="label">USERNAME</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>PASSWORD</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
-      </div>
+        <div className="splash">
+          <img className="login_img" src={login_img} />
+        <div className="title">
+            <h1>Ancient Emeralds</h1>
+        <h6 className="sub-title">
+          Culture from the Past
+        </h6> 
+        </div>
+        </div>
+        <div className="container">
+          <form className="login_form" onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" bsSize="large">
+              <ControlLabel className="label">USERNAME</ControlLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+              <ControlLabel>PASSWORD</ControlLabel>
+              <FormControl
+                value={this.state.password}
+                onChange={this.handleChange}
+                type="password"
+              />
+            </FormGroup>
+            <Button
+              block
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+            >
+              SIGN IN
+            </Button>
+          </form>
+          <div className="footer">
+          <span className="left">
+            FORGOT PASSWORD
+          </span>
+          <span className="right">
+            SIGN UP
+          </span>
+          </div>
+        </div>
       </div>
     );
   }
