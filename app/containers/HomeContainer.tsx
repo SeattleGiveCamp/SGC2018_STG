fb6e322a643665420ba023891ad82cd0e2b05d99
neelip <neelip@uw.edu>
2018-10-27 20:19:52 -0700

    Api calls and working links

--- app/containers/HomeContainer.tsx
+++ app/containers/HomeContainer.tsx
@@ -34,19 +34,14 @@ class HomeContainer extends React.Component<Props, {}> {
 	render() {
 		const missions = this.props.data.missions;
 
-		const missionMap = ({missions}) => (
-			<div>
-				{missions.map(mission => (
-					<div className="col-sm-4" key={mission.MissionID}>{mission.MissionID}
-						<p key={mission.MissionID}>{mission.MissionID}</p>
-						<img onClick={()=>this.handleContent("abc")} width="100%" src={mission.img_url} alt="Image" key={mission.img_url}/>
-						<p key={mission.MissionID}>{</p>
+		const missionMap = missions.map(mission => (
+					<div className="col-sm-4" key={mission.MissionID}>
+						<p key={mission.MissionID}>{mission.MissionName}</p>
+						<img onClick={()=>this.handleContent(mission.MissionID)} width="100%" src={mission.img_url} alt="Image" key={mission.MissionID}/>
 					</div>
 				))
-
-				}
-			</div>
-		);
+		
+		
 		return (
 			<div>
 				<h1>STG Scavenger Hunt Game Goes Here</h1>
@@ -54,18 +49,7 @@ class HomeContainer extends React.Component<Props, {}> {
 				<div className="container-fluid bg-3 text-center"> 
   					<h3>Where To Find Me?</h3>
   					<div className="row">
-    					<div className="col-sm-4" id="111">
-      						<p>Lorem ipsum..</p>
-      						<img onClick={()=> this.handleContent("abc")} width="100%" src={art} alt="Image"/>
-    					</div>
-    					<div className="col-sm-4" id="222">
-      						<p>Lorem ipsum..</p>
-      						<img onClick={()=> this.handleContent("222")} width="100%" src={music} alt="Image"/>
-    					</div>
-    					<div className="col-sm-4" id="333"> 
-      						<p>Lorem ipsum..</p>
-      						<img onClick={() => this.handleContent("333")} width="100%" src={history} alt="Image"/>
-    					</div>
+    					{missionMap}
   					</div>
 				</div>
 			</div>
