d8881d4bf4d388291e87ab472750eb3c6572cb7f
Donnie <donnie86@gmail.com>
2018-10-27 21:30:00 -0700

    fixed qr ids

--- app/containers/QRReaderContainer.tsx
+++ app/containers/QRReaderContainer.tsx
@@ -4,9 +4,14 @@ import QRScanner from '../components/QRScanner';
 import { Link, RouterStore } from 'mobx-router';
 import routes from '../routes';
 
+//import { ReactCSSTransitionGroup } from 'react-transition-group' // ES6
+//var ReactCSSTransitionGroup = require('react-transition-group'); // ES5 with npm
+
 interface State {
   displayQrData: string;
   redirectId: string;
+  fading: boolean;
+  msg: string;
 }
 interface Props {
 	store?: RouterStore;
@@ -19,13 +24,16 @@ class App extends React.Component<Props, State> {
     private camera;
     private img;
     private src;
+    private timer;
     
   constructor(props) {
     super(props);
 
     this.state = {
       displayQrData: null,
-      redirectId: null
+      redirectId: null,
+      fading: false,
+      msg: null
 		};
   }
 
@@ -38,26 +46,48 @@ class App extends React.Component<Props, State> {
         {
           this.setState({ displayQrData: retrievedQrData });
 
+          if (retrievedQrData.indexOf("stgpresents.org") < 0)
+          {
+            this.showError("Sorry, this isn't an STG code.");
+            return;
+          }
+
           //Extract the ID.
-          var indexOfID = retrievedQrData.indexOf("id=");
+          var queryStringParameterName = "appcontent=";
+          var indexOfID = retrievedQrData.indexOf(queryStringParameterName);
           if (indexOfID >= 0)
           {
-            var justTheID = retrievedQrData.substring(indexOfID + 3);
-            this.setState({ redirectId: justTheID});
+            var justTheID = retrievedQrData.substring(indexOfID + (queryStringParameterName.length));
             if(justTheID != null){
               this.props.store.router.goTo(routes.pointpage,{id:justTheID});
             }
           }
-         //this.props.store.router.goTo(routes.pointpage,{id:'abc'});
-          
-        }
+         //this.props.store.router.goTo(routes.pointpage,{id:'abc'}); 
+        } 
+      }
+      else //
+      {
+        
       }
     
 
   }
 
+ showError(message) {
+    // // in some handler code
+    // this.setState({fading: true}); // fade out
+    // this.timer = setTimeout(_ => {
+    //   this.setState({msg: message}); // swap the text
+    //   this.setState({fading: false, msg: ""}); // fade back in
+    // }, 3000); // animation timing offset
+
+    this.setState({msg: message});
+ }
 
   render() {
+
+    const {msg, fading} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -74,7 +104,17 @@ class App extends React.Component<Props, State> {
           <div>
              ID: {this.state.redirectId}
           </div>
-
+          
+          {/* <ReactCSSTransitionGroup
+            transitionName="errorSection"
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+            transitionEnter={false}
+            transitionLeave={false}
+            transitionLeaveTimeout={2500}>
+            <h4>{this.state.msg}</h4>
+          </ReactCSSTransitionGroup> */}
+          <span id="error">{this.state.msg}</span>
           <a
             className="App-link"
             href="https://www.stgpresents.org/"
